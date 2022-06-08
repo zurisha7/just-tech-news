42cc0bfc37f6852f37bca596cb35ef64d3bc8fc2
Zurisha <andreamwest7@gmail.com>
2022-06-07 19:48:45 -0500

    added login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -46,6 +46,27 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
 
 router.post('/login', (req, res) => {
   User.findOne({
