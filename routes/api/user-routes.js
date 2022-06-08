6584aee38a2e75441c3b92bf47931f1ef1958be0
Zurisha <andreamwest7@gmail.com>
2022-06-07 19:11:44 -0500

    hashed password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -47,11 +47,34 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(dbUserData => {
+    if(!dbUserData){
+     res.status(400).json({ message: 'No user with that email address!' });
+     return;
+    }
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if(!validPassword){
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+   res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+    
+  });
+});
+
 router.put('/:id', (req, res) => {
   //expects {username: '', email: '', password: ''}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
