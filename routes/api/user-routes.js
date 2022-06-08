3b2ae81f4d424145043c7c07c942e88bc6d3d45c
Zurisha <andreamwest7@gmail.com>
2022-06-08 13:40:12 -0500

    post routers

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -18,7 +18,13 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -34,7 +40,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -46,6 +52,7 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
@@ -59,6 +66,7 @@ router.post('/login', (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
@@ -68,30 +76,8 @@ router.post('/login', (req, res) => {
   });
 });
 
-router.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-  .then(dbUserData => {
-    if(!dbUserData){
-     res.status(400).json({ message: 'No user with that email address!' });
-     return;
-    }
-    const validPassword = dbUserData.checkPassword(req.body.password);
-    if(!validPassword){
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
-   res.json({ user: dbUserData, message: 'You are now logged in!' });
-
-    
-  });
-});
-
 router.put('/:id', (req, res) => {
-  //expects {username: '', email: '', password: ''}
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
